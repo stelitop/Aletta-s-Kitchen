7fe38af8de4cfacfebaa1d253a862073f5cee076
stelitop <stilijan9@gmail.com>
2021-05-23 20:13:56 +0300

    Testing if the Discord Webhook works.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,33 @@ namespace Aletta_s_Kitchen
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ButtonBotStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonBotStart
+            // 
+            this.ButtonBotStart.Location = new System.Drawing.Point(107, 118);
+            this.ButtonBotStart.Name = "ButtonBotStart";
+            this.ButtonBotStart.Size = new System.Drawing.Size(115, 35);
+            this.ButtonBotStart.TabIndex = 0;
+            this.ButtonBotStart.Text = "Start The Bot";
+            this.ButtonBotStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonBotStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonBotStart;
     }
 }
 
