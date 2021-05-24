4d3029d32c40b2dfc2cfb0042d1bfb0e079f3926
stelitop <stilijan9@gmail.com>
2021-05-24 11:26:28 +0300

    Added the files for the most basic class structure.

---
+++ GameRelated/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aletta_s_Kitchen.GameRelated
+{
+    public class Game
+    {
+        int curRound;
+
+    }
+}
---
+++ GameRelated/IngredientRelated/EffectRelated/EffectType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aletta_s_Kitchen.GameRelated.IngredientRelated.EffectRelated
+{
+    enum EffectType
+    {
+		OnBeingPicked,
+		OnBeingCooked,
+		WheneverThisGainsPoints,
+		WheneverAddPointsToScore,
+		OnBuyingAnIngredient,
+		OnEnteringKitchen,
+		Deathrattle
+	}
+}
